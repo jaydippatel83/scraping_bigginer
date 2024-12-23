6b7b00215be23279d8c900a2aaa27a2031c71357
jaydippatel83 <jaydippatel83.pj@gmail.com>
2024-12-23 18:00:11 +0530

    print data

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ with open('index.html', 'r') as file:
 soup = BeautifulSoup(html_content, 'lxml') 
 print(soup.prettify())
 cards = soup.find_all('div', class_='card')
+
 for card in cards:
     print(card.find('h2').text)
     print(card.find('p').text)
     print(card.find('a').text)
-    print(card.find('a')['href'])
