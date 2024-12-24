521979ac1a077895394b65414f26b430594b99cf
jaydippatel83 <jaydippatel83.pj@gmail.com>
2024-12-24 14:26:49 +0530

    fix issue

--- index.py
+++ index.py
@@ -5,9 +5,25 @@ url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personaliz
 response = requests.get(url).text
 
 soup = BeautifulSoup(response, 'lxml')
-jobs_cards = soup.find('li', class_='clearfix job-bx wht-shd-bx')
 
-for job_card in jobs_cards: 
-    job_company = job_card.find('h2', class_='heading-trun').text
-    job_location = job_card.find('ul', class_='list-job-dtl clearfix').find('li').text
-    print(job_company, job_location)
+# Find all job listings
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+# Loop through each job
+for job in jobs:
+    published_date = job.find('span', class_='sim-posted').span.text
+
+    # Filter jobs posted a few days ago
+    if 'few' in published_date:
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        skills_element = job.find('span', class_='srp-skills')
+        if skills_element:
+            skills = skills_element.text.replace(' ', '')
+        else:
+            skills = None  # Handle the case where the element is not found
+        more_info = job.header.h2.a
+
+        # Print the extracted details
+        print(f"Company Name: {company_name}")
+        print(f"Required Skills: {skills}")
+        print(f"More Info: {more_info.attrs['href']}")
