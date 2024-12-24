6576fa5c0ebcc42ccf68a9e45783e75f00ecd55e
jaydippatel83 <jaydippatel83.pj@gmail.com>
2024-12-24 14:56:02 +0530

    write file

--- index.py
+++ index.py
@@ -1,29 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=ft&searchTextText=&txtKeywords=blockchain&cboWorkExp1=2&txtLocation='
-response = requests.get(url).text
+print("Enter the job title: ")
+job_title = input('>')
+print("Enter the location: ")
+location = input('>')
+print('Searching for jobs...')
 
-soup = BeautifulSoup(response, 'lxml')
+def get_jobs():
+    
+    url = f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=ft&searchTextText=&txtKeywords={job_title}&cboWorkExp1=2&txtLocation={location}'
+    response = requests.get(url).text
 
-# Find all job listings
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    soup = BeautifulSoup(response, 'lxml')
 
-# Loop through each job
-for job in jobs:
-    published_date = job.find('span', class_='sim-posted').span.text
+    # Find all job listings
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
-    # Filter jobs posted a few days ago
-    if 'few' in published_date:
-        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-        skills_element = job.find('span', class_='srp-skills')
-        if skills_element:
-            skills = skills_element.text.replace(' ', '')
-        else:
-            skills = None  # Handle the case where the element is not found
-        more_info = job.header.h2.a
+    # Loop through each job
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_='sim-posted').span.text
 
-        # Print the extracted details
-        print(f"Company Name: {company_name}")
-        print(f"Required Skills: {skills}")
-        print(f"More Info: {more_info.attrs['href']}")
+         # Filter jobs posted a few days ago
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills_element = job.find('span', class_='srp-skills')
+            if skills_element:
+                 skills = skills_element.text.replace(' ', '')
+            else:
+                 skills = None  # Handle the case where the element is not found
+            more_info = job.header.h2.a
+            
+            if skills is not None:
+                 with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f"Company Name: {company_name}\n")
+                    f.write(f"Required Skills: {skills}\n")
+                    f.write(f"More Info: {more_info.attrs['href']}\n")
+
+                 # Print the extracted details
+            print(f"Job {index + 1} details saved to posts/{index}.txt")
+if __name__ == '__main__':
+    while True:
+        get_jobs()
+        time_wait = 1
+        print(f'Waiting {time_wait} seconds...')
+        time.sleep(time_wait * 60)
